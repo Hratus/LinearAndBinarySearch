740e9b5530a6cdec964e352609cdb7e30fd7accf
HRATUS <rhatuz29@gmail.com>
2025-02-07 19:21:21 -0300

    Numero de passos de cada busca, adicionado

--- src/main/java/org/Hratus/Main.java
+++ src/main/java/org/Hratus/Main.java
@@ -3,14 +3,14 @@ package org.Hratus;
 public class Main {
     public static void main(String[] args) {
 
-        int nums[] = {5,6,7,8,9};
+        int nums[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
 
-        int target = 5;
+        int target = 12;
         int result = linearSearch(nums, target);
         int result2 = binarySearch(nums, target);
         if(result != -1) {
             System.out.println("Elemento encontrado: " + result2);
-            System.out.println(nums.length);
+
         }
         else {
             System.out.println("Elemento nao encontrado");
@@ -21,25 +21,33 @@ public class Main {
 
     //Busca Linear
     public static int linearSearch(int[] nums, int target) {
+        int steps=0;
         for (int i = 0; i < nums.length; i++) {
+            steps++;
             if (nums[i] == target) {
+                System.out.println("o numero de passos de busca linear foram: " + steps);
                 return i;
             }
         }
        return -1;
+
     }
 
     public static int binarySearch(int[] nums, int target) {
         int esquerda = 0;
         int direita = nums.length - 1;
+        int steps = 0;
 
         //{5,6,7,8,9};
         //esquerda é o index inicial da matriz, direita é o ultimo index a partir do 0
         while (esquerda <= direita) {
+            steps++;
             int meio = (esquerda + direita) / 2;
             //Se o meio for compadado com o algo, ele retorna o meio
             if (nums[meio] == target) {
+                System.out.println("o numero de passos de busca binaria foram: " + steps);
                 return meio;
+
 
             }
             //caso o meio foi menor que o alvo e o alvo e esteja a direita. A esqueda passa a ser o meio+1, removendo os numeros a esquerda do novo meio
@@ -50,6 +58,7 @@ public class Main {
             else {
                 direita = meio - 1;
             }
+
         }
 
 
